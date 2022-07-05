321083c7af396729c88b7516689906787e31921b
Jakob Ruckel <jakob@ruckel.de>
2022-07-05 15:14:26 +0200

    Work on migration to new input system

--- Assets/Scripts/Ship/ShipManager.cs
+++ Assets/Scripts/Ship/ShipManager.cs
@@ -230,6 +230,7 @@ public class ShipManager : NetworkBehaviour {
     }
 
     private void Update() {
+        /* TODO MIGRATE TO INPUTSYSTEM
         if(Input.GetKeyDown(KeyCode.P) && IsServer){
             if (HasPower) {
                 TriggerPowerOutageEvent();
@@ -250,10 +251,12 @@ public class ShipManager : NetworkBehaviour {
                 Debug.Log(room.Name + ": " + room.RoomOxygen);
             }
         }
+        */
         
         CheckWinCondition();
 
         if (IsServer) {
+            /* TODO MIGRATE TO INPUTSYSTEM
             if (Input.GetKey(KeyCode.UpArrow)){
                 m_Speed.Value += 0.1f;
             }
@@ -268,6 +271,7 @@ public class ShipManager : NetworkBehaviour {
             if (Input.GetKey(KeyCode.RightArrow)){
                 m_Rotation.Value -= 1f;
             }
+            */
 
             UpdatePosition();
         }
