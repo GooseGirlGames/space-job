9f0afcb2cd248ca4367fab2987897586dd50f09d
Jakob Ruckel <jakob@ruckel.de>
2022-07-09 21:32:46 +0200

    Restore remaining broken inputs

--- Assets/Scripts/Ship/ShipManager.cs
+++ Assets/Scripts/Ship/ShipManager.cs
@@ -230,7 +230,7 @@ public class ShipManager : NetworkBehaviour {
     }
 
     private void Update() {
-        /* TODO MIGRATE TO INPUTSYSTEM
+#if !DISABLE_DEBUG_KEYS
         if(Input.GetKeyDown(KeyCode.P) && IsServer){
             if (HasPower) {
                 TriggerPowerOutageEvent();
@@ -251,12 +251,12 @@ public class ShipManager : NetworkBehaviour {
                 Debug.Log(room.Name + ": " + room.RoomOxygen);
             }
         }
-        */
+#endif
         
         CheckWinCondition();
 
         if (IsServer) {
-            /* TODO MIGRATE TO INPUTSYSTEM
+#if !DISABLE_DEBUG_KEYS
             if (Input.GetKey(KeyCode.UpArrow)){
                 m_Speed.Value += 0.1f;
             }
@@ -271,7 +271,7 @@ public class ShipManager : NetworkBehaviour {
             if (Input.GetKey(KeyCode.RightArrow)){
                 m_Rotation.Value -= 1f;
             }
-            */
+#endif
 
             UpdatePosition();
         }
